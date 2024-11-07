8e38c9c42749c1792ab92205c3d6762be49eac33
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-11-07 17:36:36 -0300

    props

--- src/Containers/ListaDeContatos/index.tsx
+++ src/Containers/ListaDeContatos/index.tsx
@@ -1,13 +1,22 @@
 import Contato from '../../components/Contato'
 import { Container } from './styles'
 
+const contatos = [
+  {
+    nome: 'User',
+    telefone: '0000',
+    email: 'user@user.com'
+  }
+]
 const ListaDeContatos = () => (
   <Container>
     <p>Você possui x contatos salvos</p>
     <ul>
-      <li>
-        <Contato />
-      </li>
+      {contatos.map((c) => (
+        <li key={c.nome}>
+          <Contato nome={c.nome} telefone={c.telefone} email={c.email} />
+        </li>
+      ))}
     </ul>
   </Container>
 )
--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Contato = () => (
-  <S.Card>
-    <S.Name type="text" placeholder="Nome do Contato" />
-    <S.Info type="tel" placeholder="Telefone do Contato" />
-    <S.Info type="email" placeholder="Email do Contato" />
-    <S.Actions>
-      <S.Buttons>Editar</S.Buttons>
-      <S.Buttons>Remover</S.Buttons>
-    </S.Actions>
-  </S.Card>
-)
+type Props = {
+  nome: string
+  telefone: string
+  email: string
+}
+
+const Contato = ({ nome, telefone, email }: Props) => {
+  const [isEditing, setEditing] = useState(false)
+  return (
+    <S.Card>
+      <S.Name type="text" placeholder="Nome do Contato" value={nome} />
+      <S.Info type="tel" placeholder="Telefone do Contato" value={telefone} />
+      <S.Info type="email" placeholder="Email do Contato" value={email} />
+      <S.Actions>
+        {isEditing ? (
+          <>
+            <S.Buttons>Salvar</S.Buttons>
+            <S.Buttons onClick={() => setEditing(false)}>Cancelar</S.Buttons>
+          </>
+        ) : (
+          <>
+            <S.Buttons onClick={() => setEditing(true)}>Editar</S.Buttons>
+            <S.Buttons>Remover</S.Buttons>
+          </>
+        )}
+      </S.Actions>
+    </S.Card>
+  )
+}
+
 export default Contato
